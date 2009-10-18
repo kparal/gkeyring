9ae4419007e82fe0d9a66d606d09dd7e4ffcb346
Kamil Páral <kamil.paral@gmail.com>
2009-10-18 22:12:00 +0200

    complete rework, now it can handle keyring items of any type, not only network ones

--- gkeyring.py
+++ gkeyring.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# A small Python tool for command-line access to GNOME keyring. It provides
-# simple querying for and storing of network credentials. It can be used as
-# a CLI or as a Python module.
+# A small Python tool for shell access to GNOME keyring. It provides
+# simple querying for and creating of keyring items.
 #
 # Author: Kamil Páral <kamil.paral /at/ gmail /dot/ com>, 2009
 #
 # This program is free software, licensed under GNU AGPL 3:
 # http://www.gnu.org/licenses/agpl-3.0.html
-#
-# Based on the work of Sebastian Rittau: http://www.rittau.org/blog/20070726-01
 
-import sys, optparse, getpass, traceback
+import sys, optparse, getpass
 import gtk # ensure that the application name is correctly set
-import gnomekeyring as gkey
+import gnomekeyring as gk
 
 _version = '0.1.0.99'
 
 class CLI(object):
-    ''' Class providing command-line interface '''
-    # operational modes
-    unknown = 0
-    SET = 1
-    GET = 2
+    ''' Class providing command-line interface for GNOME keyring '''
+
+    ITEM_TYPES = {
+        'generic': gk.ITEM_GENERIC_SECRET,
+        'network': gk.ITEM_NETWORK_PASSWORD,
+        'note': gk.ITEM_NOTE
+        }
 
     def __init__(self):
-        self.keyring = Keyring()
-        self.mode = CLI.unknown
         self.options = None
-        self.args = None
+        self.keyring = None
+        self.params = {}
+        self.id = None
+        self.item_type = None
+        self.secret = None
+        self.name = None
+        self.output = None
 
     def parse_args(self):
         ''' Parse commandline options.
@@ -38,66 +41,135 @@ class CLI(object):
         '''
 
         desc=\
-"""Use 'get' for retrieving network credentials from GNOME keyring and 'set'
-for storing network credentials into the keyring.
+'''By default %prog queries the GNOME keyring for items matching specified
+arguments. You can define the item exactly by --id, or search for it using
+-p and/or -i.
 
-If you are getting credentials, they will be in the form:
-server [TAB] user [TAB] password
-If there are multiple results matching your query, they will be on multiple
-lines."""
+It is important to understand that the keyring items are divided into several
+types (see --type) and each contain several properties. Both the item type and
+item properties can be inspected using a tool like Seahorse. Then you will know
+what to query for.
+
+Zero or more items may match your query. They will be printed out one item
+a line, by default in the format:
+ID [TAB] secret
+
+You can also create a new keyring item using --set. In this case the arguments
+-p and/or -i are mandatory and will be used as properties of the new item.
+
+When a new keyring item is created, its ID is printed out on the output.'''
 
         parser = MyOptionParser(description=desc, version=_version)
-        parser.epilog = 'Example usage: %s get -s myserver.com -p ftp' \
-% parser.get_prog_name()
 
-        parser.set_usage('%prog [get|set] [options]')
+        parser.add_option('-t', '--type', choices=CLI.ITEM_TYPES.keys(), 
+            default='generic', help='type of keyring item: '
+            'generic, network or note [default: %default]')
+        parser.add_option('-k', '--keyring', help='keyring name [default: '
+            'default keyring]', dest=self.keyring)
+        parser.add_option('--id', type='int', help='key ID')
+        parser.add_option('-p', default='',
+            metavar='PARAM1=VALUE1,PARAM2=VALUE2', dest='params',
+            help='params and values of keyring item, e.g. user, server, '
+            'protocol, etc.')
+        parser.add_option('-i', default='', 
+            metavar='PARAM1=VALUE1,PARAM2=VALUE2', dest='params_int', 
+            help='same as -p, but values are treated as integers, not strings')
+    
+        out_group = optparse.OptionGroup(parser, 'Formatting output for '
+            'querying keyring items')
+        out_group.add_option('-o', '--output', default='id,secret',
+            help='comma-separated list of columns to be printed on the output.'
+            " Column name may include any name of item's property or keywords "
+            "'id', 'secret' and 'name'. Columns will be separated by tabs. "
+            "[default: %default]")
+        out_group.add_option('-1', action='store_true', dest='output1',
+            help="same as '--output secret'")
+        parser.add_option_group(out_group)
 
-        get_group = optparse.OptionGroup(parser,
-                                         "Options to be used for 'get'")
-        get_group.add_option('-s', '--server', help='network server')
-        get_group.add_option('-p', '--protocol', help='network protocol')
-        get_group.add_option('-u', '--user', help='username')
-
-        set_group = optparse.OptionGroup(parser,
-                    "Options to be used for 'set'", "Notice: Also all "
-                    "options for 'get' can be used here. Mandatory options "
-                    "are: protocol, server, user.")
-        set_group.add_option('-w', '--password', help='password (you will be '
-                             'asked if not specified)')
-        set_group.add_option('-t', '--port', help='network port')
-        set_group.add_option('-d', '--domain', help='network domain')
-        set_group.add_option('-k', '--keyring', help='keyring name (will use '
-                             'default one when no specified)')
-
-        parser.add_option_group(get_group)
+        set_group = optparse.OptionGroup(parser, 'Options specific for '
+            'creating keyring items')
+        set_group.add_option('-s', '--set', action='store_true', default=False, 
+            help='create a new item in the keyring instead of querying')
+        set_group.add_option('-n', '--name', 
+            help='keyring item descriptive name [mandatory if --set]')
+        set_group.add_option('-w', '--password', help='keyring item password')
         parser.add_option_group(set_group)
 
-        try:
-            if sys.argv[1] == 'set':
-                self.mode = CLI.SET
-                del sys.argv[1]
-            elif sys.argv[1] == 'get':
-                self.mode = CLI.GET
-                del sys.argv[1]
-        except IndexError:
-            pass
+        epilog=\
+"""Example usage:
+$ %(prog)s --id 12
+Get keyring item with ID 12 in default keyring.
+
+$ %(prog)s -p account_name=my@jabber.org -i gajim=1 -1
+Search for keyring item with property 'account_name' with value 'my@jabber.org'
+and property 'gajim' with integer value '1'. Output only the secret(s).
+
+$ %(prog)s --type network --p server=my.com,protocol=ftp --output user,secret
+Search for network keyring item with 'server' and 'protocol' properties. Output
+property 'user' followed by item's secret.
+
+$ %(prog)s --set --name 'foo' -p bar=baz --keyring login
+Create a new item in keyring 'login' with name 'foo' and property 'bar'."""
+        parser.epilog = epilog % {'prog': parser.get_prog_name()}
 
         (options, args) = parser.parse_args()
-        self.options, self.args = options, args
+        self.options = options
 
-        if self.mode == CLI.unknown:
-            parser.error("Must specify 'get' or 'set' as first argument! "
-                         'See --help.')
+        if not gk.is_available():
+            print >>sys.stderr, 'GNOME keyring is not available!'
+            return False
 
-        if self.mode == CLI.SET:
+        if not options.params and not options.params_int and \
+        (options.set or not options.id):
+            parser.error('Missing option -p or -i! See --help.')
+
+        # parse string params
+        try:
+            tuples = options.params.split(',')
+            for tupl in tuples:
+                if not tupl:
+                    # empty string
+                    continue
+                name, val = tupl.split('=',1)
+                self.params[name] = val
+        except ValueError, e:
+            parser.error('Incorrect syntax of "-p param1=value1,'
+                'param2=value2"! See --help.\nDetails:\n%s' % e)
+
+        # parse integer params
+        try:
+            tuples = options.params_int.split(',')
+            for tupl in tuples:
+                if not tupl:
+                    # empty string
+                    continue
+                name, val = tupl.split('=',1)
+                val = int(val)
+                self.params[name] = val
+        except ValueError, e:
+            parser.error('Incorrect syntax of "-i param1=value1,'
+                'param2=value2"! See --help.\nDetails:\n%s' % e)
+
+        if options.keyring:
+            self.keyring = options.keyring
+        else:
+            self.keyring = gk.get_default_keyring_sync()
+            
+        if options.output1:
+            options.output = 'secret'
+
+        if options.set:
+            if not options.name:
+                parser.error('Missing --name! See --help.')
             if not options.password:
                 # ask for password
                 options.password = getpass.getpass()
-            # check mandatory options
-            set_mandatory = ['server', 'protocol', 'user', 'password']
-            for mand in set_mandatory:
-                if options.__dict__[mand] is None:
-                    parser.error('Mandatory option is missing! See --help.')
+        
+        self.secret = options.password
+        self.id = options.id
+        self.name = options.name
+        self.item_type = CLI.ITEM_TYPES[options.type]
+        self.output = options.output.split(',')
 
         return True
 
@@ -107,98 +179,77 @@ lines."""
         if not self.parse_args():
             sys.exit(2)
 
-        if not gkey.is_available():
-            print >>sys.stderr, "GNOME keyring is not available!"
-            sys.exit(5)
-
-        if self.mode == CLI.GET:
-            self._query()
-        elif self.mode == CLI.SET:
-            self._create()
-
-        sys.exit(0)
-
-    def _query(self):
-        ''' Get network credentials from keyring '''
-
-        o = self.options
-        matches = self.keyring.get_credentials(o.server, o.protocol, o.user)
-        if not matches:
-            sys.exit(3)
-
-        for match in matches:
-            line = '%s://%s' % (match['protocol'], match['server'])
-            if 'port' in match:
-                line += ':%s' % match['port']
-            line += '\t%s\t%s' % (match['user'], match['password'])
-            print line
-
-    def _create(self):
-        ''' Store network credentials into keyring '''
-
-        o = self.options
-        try:
-            self.keyring.set_credentials(o.server, o.protocol, o.user,
-                                o.password, o.port, o.domain, o.keyring)
-        except KeyringError as e:
-            print >>sys.stderr, 'Error creating keyring item'
-            traceback.print_exc(file=sys.stderr)
-            sys.exit(4)
-
-
-class Keyring(object):
-    '''
-    Class for accessing GNOME keyring items containing network passwords.
-    '''
-
-    def has_credentials(self, server=None, protocol=None, user=None):
-        ''' Check whether some credentials exist matching specified parameters.
-
-        Returns True or False. Always returns False if no parameter specified.
-        '''
-
-        items = self.get_credentials(server=server, protocol=protocol,
-                                     user=user)
-        return len(items) > 0
-
-    def get_credentials(self, server=None, protocol=None, user=None):
-        ''' Get all credentials matching specified parameters.
-
-        Returns list of matches. Each match is a dictionary. Returns empty list
-        if nothing matching found. Always returns empty list if no parameter
-        specified.
-        '''
-
-        try:
-            attrs = {'server': server, 'protocol': protocol, 'user': user}
-            items = gkey.find_network_password_sync(**attrs)
-        except gkey.Error:
-            return []
+        if self.options.set:
+            ret = self.create()
         else:
-            return items
+            ret = self.query()
 
-    def set_credentials(self, server, protocol, user, password, port=None,
-                        domain=None, keyring=None):
-        ''' Create a new item in the keyring.
+        sys.exit(0 if ret else 5)
 
-        A default keyring is used if no keyring is specified. A KeyringError
-        is thrown if item can't be created.
+    def query(self):
+        ''' Match keyring items from keyring.
+
+        Return True if something was matched, False otherwise.
+        '''
+
+        results = []
+
+        try:
+            if self.id:
+                info = gk.item_get_info_sync(self.keyring, self.id)
+                attr = gk.item_get_attributes_sync(self.keyring, self.id)
+                result = {'id': self.id, 'secret': info.get_secret(),
+                          'name': info.get_display_name(), 'attr': attr}
+                results.append(result)
+            else:
+                matches = gk.find_items_sync(self.item_type, self.params)
+                for match in matches:
+                    result = {'id': match.item_id, 'secret': match.secret,
+                              'attr': match.attributes}
+                    if 'name' in self.output:
+                        # do this only when required, because it pops up
+                        # one more 'allow access?' dialog
+                        info = gk.item_get_info_sync(self.keyring, 
+                                                     match.item_id)
+                        result['name'] = info.get_display_name()
+                results.append(result)
+        except gk.Error:
+            pass
+
+        if not results:
+            return False
+
+        for result in results:
+            for tab in self.output:
+                if tab == 'id':
+                    print result['id'],
+                elif tab == 'secret':
+                    print result['secret'],
+                elif tab == 'name':
+                    print result['name'],
+                elif tab in result['attr']:
+                    print result['attr'][tab],
+                print '\t',
+            print
+            
+        return True
+
+    def create(self):
+        ''' Create a new keyring item.
+
+        Return True if item created successfully, False otherwise.
         '''
 
         try:
-            def_keyring = gkey.get_default_keyring_sync()
-            attrs = {'server': server, 'protocol': protocol, 'user': user,
-                     'password': password, 'domain': domain,
-                     'keyring': keyring or def_keyring}
-            if port:
-                attrs['port'] = int(port)
-            gkey.set_network_password_sync(**attrs)
-        except (gkey.Error, TypeError) as e:
-            raise KeyringError('Error setting credentials', e)
+            id = gk.item_create_sync(self.keyring, self.item_type, self.name,
+                                     self.params, self.secret, False)
+        except gk.Error, e:
+            print >>sys.stderr, 'Error creating keyring item!\nDetails:\n%s'%e
+            return False
+            
+        print id
 
-
-class KeyringError(Exception):
-    ''' Base exception for all errors coming from this module. '''
+        return True
 
 
 class MyOptionParser(optparse.OptionParser):
@@ -206,10 +257,10 @@ class MyOptionParser(optparse.OptionParser):
     the native formatting does not respect newlines.
     '''
     def format_description(self, formatter):
-        return self.get_description() + '\n'
-        
+        return (self.get_description() or '') + '\n'
+
     def format_epilog(self, formatter):
-        return '\n' + self.epilog + '\n'
+        return '\n' + (self.epilog or '') + '\n'
 
 
 if __name__ == '__main__':
